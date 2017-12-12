16d0d188fbdbf6aa76eb4a80969ba1703b437ef7
Eric Arellano <ericarellano@me.com>
2017-12-11 20:52:51 -0700

    add console

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,10 @@
-import IO.CommandLine
+import IO.{CommandLine, Console}
 import Models.Message
 
 object App extends App {
-  val m: Message = CommandLine.interpret(args.toList)
+//  val m: Message = CommandLine.interpret(args.toList)
+//  println(m)
+
+  val m: Message = Console.consoleApp()
   println(m)
 }
---
+++ src/main/scala/IO/Console.scala
@@ -0,0 +1,36 @@
+package IO
+
+import Cipher.Cipher
+import Models.{Key, Message}
+
+import scala.io.StdIn
+
+case class Instruction(value: String) extends AnyVal
+
+object Console {
+
+  def instruct(instr: Instruction): Unit =
+    println(instr.value)
+
+  def readNumber(validation: Int => Boolean = _ => true): Int = {
+    val i = StdIn.readInt()
+    if (validation(i)) i else readNumber(validation)
+  }
+
+  def readString(validation: String => Boolean = _ => true): String = {
+    val s = StdIn.readLine()
+    if (validation(s)) s else readString(validation)
+  }
+
+  def consoleApp(): Message = {
+    Console.instruct(Instruction("Input 1 to decrypt or 2 to encrypt: "))
+    val inputMode = Console.readNumber(num => num == 1 || num == 2)
+    val mode = if (inputMode == 1) CipherMode.Decrypt else CipherMode.Encrypt
+    Console.instruct(Instruction("Input your key. This can only contain letters a-Z: "))
+    val key = Key(Console.readString(s => s.forall(c => c.isLetter && c <= 'z')))
+    Console.instruct(Instruction("Input the message: "))
+    val message = Message(Console.readString())
+    if (mode == CipherMode.Decrypt) Cipher.decrypt(message, key) else Cipher.encrypt(message, key)
+  }
+
+}
